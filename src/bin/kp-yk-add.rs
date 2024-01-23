ec926ef5ce5ab52fbf2d33a15803431912167947
louib <code@louib.net>
2024-01-22 23:44:15 -0500

    feat: allow empty passwords for yk utilities (#210)

--- src/bin/kp-yk-add.rs
+++ src/bin/kp-yk-add.rs
@@ -25,6 +25,10 @@ struct Args {
     /// Provide a keyfile
     #[arg(short = 'k', long)]
     keyfile: Option<String>,
+
+    /// Do not use a password to decrypt the database
+    #[arg(long)]
+    no_password: bool,
 }
 
 pub fn main() -> Result<()> {
@@ -37,7 +41,13 @@ pub fn main() -> Result<()> {
         key = key.with_keyfile(&mut File::open(f)?)?;
     }
 
-    key = key.with_password_from_prompt("Password (or blank for none): ")?;
+    if !args.no_password {
+        key = key.with_password_from_prompt("Password: ")?;
+    }
+
+    if key.is_empty() {
+        return Err(anyhow::format_err!("No database key was provided."));
+    }
 
     let db = Database::open(&mut source, key.clone())?;
 
