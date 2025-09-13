856116936e9d13daacb9ec2020ed54316c047b1f
louib <code@louib.net>
2025-09-13 17:04:31 -0400

    feat: add simple nix flake for development (#268)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (
+    flake-utils.lib.eachDefaultSystem (
+      system: (
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              rust-analyzer
+            ];
+          };
+        }
+      )
+    )
+  );
+}
