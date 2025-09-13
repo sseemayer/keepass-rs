6530a56cb1cfbcaae916dd8c4a5807394ba218f7
Stefan Seemayer <stefan@seemayer.de>
2025-09-13 10:42:40 +0200

    move custom (de)serializers into module

---
+++ src/format/xml_db/custom_serde.rs
@@ -0,0 +1,51 @@
+//! Custom serde (de)serializers for specific data formats in KeePass XML flavor.
+
+/// base64-encoded binary data
+pub mod base64 {
+    use base64::{engine::general_purpose as base64_engine, Engine as _};
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(data: &[u8], s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        s.serialize_str(&base64_engine::STANDARD.encode(data))
+    }
+
+    pub fn deserialize<'de, D>(d: D) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(d)?;
+
+        base64_engine::STANDARD
+            .decode(s)
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+/// "True"/"False" boolean strings
+pub mod bool {
+
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(data: &bool, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        s.serialize_str(if *data { "True" } else { "False" })
+    }
+
+    pub fn deserialize<'de, D>(d: D) -> Result<bool, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(d)?;
+
+        match s.as_str() {
+            "True" => Ok(true),
+            "False" => Ok(false),
+            _ => Err(serde::de::Error::custom(format!("Invalid boolean string: {}", s))),
+        }
+    }
+}
