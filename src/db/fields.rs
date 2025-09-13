fd052f036d5d6abe77149cadb20e79d35df2703b
Stefan Seemayer <stefan@seemayer.de>
2025-09-13 18:00:11 +0200

    add additional fields

--- src/db/fields.rs
+++ src/db/fields.rs
@@ -1,3 +1,11 @@
+//! Standard field names for entries.
+//!
+//! This can be used with `Entry::get` and `Entry::set`.
+
 pub const TITLE: &str = "Title";
 pub const USERNAME: &str = "UserName";
 pub const PASSWORD: &str = "Password";
+pub const URL: &str = "URL";
+pub const NOTES: &str = "Notes";
+
+pub const KNOWN_FIELDS: [&str; 5] = [TITLE, USERNAME, PASSWORD, URL, NOTES];
