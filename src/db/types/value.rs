51f1a8ef01d8a70d8e2dbc20ca036c3008c1b4d6
Stefan Seemayer <stefan@seemayer.de>
2025-09-15 20:54:39 +0200

    refactor: redesign attachment system
    Have just one user-facing Attachment type, with the parsers handling the
    translation of different ways to specify attachments into this type.

--- src/db/types/value.rs
+++ src/db/types/value.rs
@@ -2,33 +2,17 @@ use secrecy::{ExposeSecret, SecretBox};
 
 #[derive(Debug)]
 pub enum Value {
-    /// unprotected binary data
-    Bytes(Vec<u8>),
-
     /// unprotected text data
     String(String),
-
-    /// protected binary data
-    PBytes(SecretBox<Vec<u8>>),
 
     /// protected text data
     PString(SecretBox<String>),
 }
 
 impl Value {
-    /// Create a new unprotected binary data value
-    pub fn bytes(data: impl Into<Vec<u8>>) -> Self {
-        Value::Bytes(data.into())
-    }
-
     /// Create a new unprotected text data value
     pub fn string(data: impl Into<String>) -> Self {
         Value::String(data.into())
-    }
-
-    /// Create a new protected binary data value
-    pub fn protected_bytes(data: impl Into<Vec<u8>>) -> Self {
-        Value::PBytes(SecretBox::new(Box::new(data.into())))
     }
 
     /// Create a new protected text data value
@@ -38,24 +22,13 @@ impl Value {
 
     /// Returns true if the value is protected (either PBytes or PString)
     pub fn is_protected(&self) -> bool {
-        matches!(self, Value::PBytes(_) | Value::PString(_))
+        matches!(self, Value::PString(_))
     }
 
     pub fn is_empty(&self) -> bool {
         match self {
-            Value::Bytes(data) => data.is_empty(),
             Value::String(data) => data.is_empty(),
-            Value::PBytes(data) => data.expose_secret().is_empty(),
             Value::PString(data) => data.expose_secret().is_empty(),
-        }
-    }
-
-    /// Returns the value as a byte slice, if it is either Bytes or PBytes
-    pub fn as_bytes(&self) -> Option<&[u8]> {
-        match self {
-            Value::Bytes(data) => Some(data),
-            Value::PBytes(data) => Some(data.expose_secret()),
-            _ => None,
         }
     }
 
@@ -64,7 +37,6 @@ impl Value {
         match self {
             Value::String(data) => Some(data),
             Value::PString(data) => Some(data.expose_secret()),
-            _ => None,
         }
     }
 }
@@ -72,9 +44,7 @@ impl Value {
 impl Clone for Value {
     fn clone(&self) -> Self {
         match self {
-            Value::Bytes(data) => Value::Bytes(data.clone()),
             Value::String(data) => Value::String(data.clone()),
-            Value::PBytes(data) => Value::PBytes(SecretBox::new(Box::new(data.expose_secret().clone()))),
             Value::PString(data) => Value::PString(SecretBox::new(Box::new(data.expose_secret().clone()))),
         }
     }
@@ -83,9 +53,7 @@ impl Clone for Value {
 impl PartialEq for Value {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Value::Bytes(a), Value::Bytes(b)) => a == b,
             (Value::String(a), Value::String(b)) => a == b,
-            (Value::PBytes(a), Value::PBytes(b)) => a.expose_secret() == b.expose_secret(),
             (Value::PString(a), Value::PString(b)) => a.expose_secret() == b.expose_secret(),
             _ => false,
         }
@@ -101,9 +69,7 @@ impl serde::Serialize for Value {
         S: serde::Serializer,
     {
         match self {
-            Value::Bytes(data) => serializer.serialize_bytes(data),
             Value::String(data) => serializer.serialize_str(data),
-            Value::PBytes(data) => serializer.serialize_bytes(data.expose_secret()),
             Value::PString(data) => serializer.serialize_str(data.expose_secret()),
         }
     }
