e6655804e857ae98f9efe9ace8f7451faeefccfe
Stefan Seemayer <stefan@seemayer.de>
2025-09-18 20:57:04 +0200

    chore: document and tweak API

--- examples/open_database.rs
+++ examples/open_database.rs
@@ -1,4 +1,4 @@
-use keepass::{db::DatabaseOpenError, Database, DatabaseKey};
+use keepass::{db::fields, Database, DatabaseKey, DatabaseOpenError};
 use std::fs::File;
 
 fn main() -> Result<(), DatabaseOpenError> {
@@ -6,6 +6,12 @@ fn main() -> Result<(), DatabaseOpenError> {
     let mut file = File::open("tests/resources/test_db_with_password.kdbx")?;
     let key = DatabaseKey::new().with_password("demopass");
     let db = Database::open(&mut file, key)?;
+
+    for entry in db.iter_all_entries() {
+        if let Some(title) = entry.get(fields::TITLE) {
+            println!("Title: {}", title);
+        }
+    }
 
     Ok(())
 }
