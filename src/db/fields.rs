6e382bed0e6732dd7b7e9a249808cb125ff07039
Stefan Seemayer <stefan@seemayer.de>
2025-09-18 19:54:19 +0200

    docs: guidance on how to use new API

--- src/db/fields.rs
+++ src/db/fields.rs
@@ -2,10 +2,20 @@
 //!
 //! This can be used with `Entry::get` and `Entry::set`.
 
+/// The title of the entry
 pub const TITLE: &str = "Title";
+
+/// The user name associated with the entry
 pub const USERNAME: &str = "UserName";
+
+/// The password associated with the entry
 pub const PASSWORD: &str = "Password";
+
+/// The primary URL associated with the entry
 pub const URL: &str = "URL";
+
+/// Notes associated with the entry
 pub const NOTES: &str = "Notes";
 
+/// Collection of known field names, for more convenient iteration
 pub const KNOWN_FIELDS: [&str; 5] = [TITLE, USERNAME, PASSWORD, URL, NOTES];
