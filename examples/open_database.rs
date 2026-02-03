88afda741be799da810d534a3c44b929cb40706e
Stefan Seemayer <stefan@seemayer.de>
2026-02-03 21:03:19 +0100

    docs: more elaborate open_database and save_database examples
    showcase various ways of using the new API for interfacing with the
    database structure

--- examples/open_database.rs
+++ examples/open_database.rs
@@ -7,9 +7,48 @@ fn main() -> Result<(), DatabaseOpenError> {
     let key = DatabaseKey::new().with_password("demopass");
     let db = Database::open(&mut file, key)?;
 
+    // Iterate over all entries and print their titles
     for entry in db.iter_all_entries() {
         println!("Title: {}", entry.get_str(fields::TITLE).unwrap_or("<no title>"));
     }
 
+    // find an entry by title
+    if let Some(entry) = db
+        .iter_all_entries()
+        .find(|e| e.get_str(fields::TITLE) == Some("asdf"))
+    {
+        println!("\nFound entry with title 'asdf'");
+
+        println!(
+            "Username: {}",
+            entry.get_str(fields::USERNAME).unwrap_or("<no username>")
+        );
+        println!(
+            "Password: {}",
+            entry.get_str(fields::PASSWORD).unwrap_or("<no password>")
+        );
+    }
+
+    // print the database structure using a recursive function
+    println!();
+    print_recursively(db.root(), 0);
+
     Ok(())
+}
+
+fn print_recursively(group: keepass::db::GroupRef, indent: usize) {
+    let indent_str = " ".repeat(indent);
+    println!("{}\u{f07b} {}", indent_str, group.name);
+
+    for subgroup in group.groups() {
+        print_recursively(subgroup, indent + 2);
+    }
+
+    for entry in group.entries() {
+        println!(
+            "{}  \u{f0b77} {}",
+            indent_str,
+            entry.get_str(fields::TITLE).unwrap_or("<no title>")
+        );
+    }
 }
--- examples/save_database.rs
+++ examples/save_database.rs
@@ -9,7 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     db.meta.database_name = Some("Demo database".to_string());
 
+    // create a group and entry using a simple API
     let mut root = db.root_mut();
+    let root_id = root.id();
 
     let mut group = root.add_group();
     group.name = "Demo group".to_string();
@@ -18,6 +20,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     entry.set(fields::TITLE, Value::string("Demo entry"));
     entry.set(fields::USERNAME, Value::string("jdoe"));
     entry.set(fields::PASSWORD, Value::protected_string("hunter2"));
+
+    // retrieve the entry ID because we cannot keep a mutable reference to `entry` around
+    let entry_id = entry.id();
+
+    // create a group, sub-group, and entry using the fluent API
+    db.root_mut()
+        .add_group()
+        .edit(|g| {
+            // g is the newly-created group
+            g.name = "Second Group".to_string();
+        })
+        .add_group()
+        .edit(|sg| {
+            // sg is the newly-created sub-group
+            sg.name = "Sub Group".to_string();
+            sg.notes = Some("Some notes".to_string());
+        })
+        .add_entry()
+        .edit(|e| {
+            e.set_unprotected(fields::TITLE, "New Entry");
+            e.set_unprotected(fields::USERNAME, "user");
+            e.set_protected(fields::PASSWORD, "pass");
+        });
+
+    assert_eq!(db.num_groups(), 4);
+    assert_eq!(db.num_entries(), 2);
+
+    // get back the first entry and verify its password
+    assert_eq!(
+        db.entry(entry_id).unwrap().get_str(fields::PASSWORD),
+        Some("hunter2")
+    );
+
+    // Use history tracking to modify and move an existing entry while tracking its history
+    db.entry_mut(entry_id)
+        .unwrap()
+        .track_changes()
+        .edit(|e| {
+            e.set_unprotected(fields::PASSWORD, "newpassword");
+        })
+        .move_to(root_id)?;
 
     #[cfg(feature = "save_kdbx4")]
     db.save(
